4e176fae9b00d7e134329299cb9c5dea9b5919d2
jdunavin <jdunavin@gmail.com>
2017-03-14 22:28:49 -0500

    20170314 Scoresheet
    Started ScoreSheet module

---
+++ ScoreSheet.py
@@ -0,0 +1,6 @@
+class ScoreSheet():
+
+	SCORE_SHEET = {'Ones':0, 'Twos':0, 'Threes:'0, 'Fours':0, 'Fives':0, 'Sixes':0, '3ofK':0, '4ofK':0, 'FH':0, 'SmStrt':0, 'LgStrt':0, 'Yahtzee':0, 'Bonus':0, 'Chance':0}
+	
+	def clearSheet(self):
+		
